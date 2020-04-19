b19c33a46fd02bea263edc8dbe9dcaecf3b09b0b
ClementATTIE <55447843+ClementATTIE@users.noreply.github.com>
2020-04-19 22:46:47 +0200

    affichage map

--- Gretagram/carte.php
+++ Gretagram/carte.php
@@ -44,6 +44,12 @@ else {
     <!--JS-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
+
+  
     <!-- Include des fonctons pouvant être utilisées + connexion BD-->
     <?php
     include("include/connect.inc.php");
@@ -87,6 +93,50 @@ else {
         <!--Fin Nav-->
         <br>
     </div>
+
+
+
+    <div id="mapid" style="width: 100%; height: 80%;"></div>
+<script>
+
+	var mymap = L.map('mapid').setView([43.602397286206816, 1.4419555664062502], 13);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(mymap);
+
+	L.marker([43.602397286206816, 1.4419555664062502]).addTo(mymap)
+    .bindPopup("<b>Hello world!</b><br />I am a popup.");
+		//.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+	/*
+    L.circle([51.508, -0.11], 500, {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5
+	}).addTo(mymap).bindPopup("I am a circle.");
+
+	L.polygon([
+		[51.509, -0.08],
+		[51.503, -0.06],
+		[51.51, -0.047]
+	]).addTo(mymap).bindPopup("I am a polygon.");
+*/
+    mymap.on('click', function(e) {
+        alert("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
+    });
+
+	var popup = L.popup();
+
+</script>
+
+
 
 
     <!--Fin Post-->
