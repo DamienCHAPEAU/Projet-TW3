d21601b72226276bdafbf0f43fa633fc83a29050
ClementATTIE <55447843+ClementATTIE@users.noreply.github.com>
2020-04-19 21:50:37 +0200

    bd

--- bd/latest_database.sql
+++ bd/latest_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 18 avr. 2020 à 23:39
+-- Généré le :  Dim 19 avr. 2020 à 19:50
 -- Version du serveur :  8.0.18
 -- Version de PHP :  7.3.12
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `db_web`
 --
+CREATE DATABASE IF NOT EXISTS `db_web` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `db_web`;
 
 -- --------------------------------------------------------
 
@@ -44,11 +46,23 @@ CREATE TABLE IF NOT EXISTS `commentaire` (
 INSERT INTO `commentaire` (`message`, `user`, `publication`, `date`) VALUES
 ('ouh', 'Coby', 'img/tilleul-arbre.jpg', '2020-04-18 00:00:00'),
 ('oké', 'Coby', 'img/tilleul-arbre.jpg', '2020-04-18 15:01:39'),
+('cool', 'Coby', 'uploads/5e9b9ba04aa85ecologie-ce-quil-faut-savoir-sur-cette-discipline-scientifique.jpg', '2020-04-19 12:59:27'),
 ('gg', 'Coby', 'uploads/5e99c6296cb42Forza3.jpg', '2020-04-18 18:50:01'),
 ('gg', 'Coby', 'uploads/5e99c6296cb42Forza3.jpg', '2020-04-18 18:51:13'),
+('cool', 'Coby', 'uploads/5e9b9ba04aa85ecologie-ce-quil-faut-savoir-sur-cette-discipline-scientifique.jpg', '2020-04-19 12:59:21'),
+('cool', 'Coby', 'uploads/5e9b9ba04aa85ecologie-ce-quil-faut-savoir-sur-cette-discipline-scientifique.jpg', '2020-04-19 12:59:17'),
+('cool', 'Coby', 'uploads/5e9b9ba04aa85ecologie-ce-quil-faut-savoir-sur-cette-discipline-scientifique.jpg', '2020-04-19 12:59:13'),
+('cool', 'Coby', 'uploads/5e9b9ba04aa85ecologie-ce-quil-faut-savoir-sur-cette-discipline-scientifique.jpg', '2020-04-19 12:59:10'),
+('cool', 'Coby', 'uploads/5e9b9ba04aa85ecologie-ce-quil-faut-savoir-sur-cette-discipline-scientifique.jpg', '2020-04-19 12:59:07'),
+('c\'est bo', 'Coby', 'uploads/5e9b9ba04aa85ecologie-ce-quil-faut-savoir-sur-cette-discipline-scientifique.jpg', '2020-04-19 12:58:55'),
+('test', 'Coby', 'uploads/5e9b9ba04aa85ecologie-ce-quil-faut-savoir-sur-cette-discipline-scientifique.jpg', '2020-04-19 12:58:34'),
+('test', 'Joel', 'uploads/5e9b71776491bTEEEST.jpg', '2020-04-19 02:31:03'),
+('test', 'Coby', 'uploads/5e9b71b7a85c9TEEEST.jpg', '2020-04-19 02:29:06'),
 ('test\r\n', 'Coby', 'uploads/5e9b6c516ae8fTEEEST.jpg', '2020-04-18 23:26:05'),
 ('test', 'defaut', 'uploads/5e9b6c516ae8fTEEEST.jpg', '2020-04-18 23:24:03'),
-('test', 'defaut', 'img/tilleul-arbre.jpg', '2020-04-18 20:47:08');
+('test', 'defaut', 'img/tilleul-arbre.jpg', '2020-04-18 20:47:08'),
+('dernier', 'Coby', 'uploads/5e9b9ba04aa85ecologie-ce-quil-faut-savoir-sur-cette-discipline-scientifique.jpg', '2020-04-19 12:59:33'),
+('ouah', 'Coby', 'uploads/5e9c2fffa5e60photo-gratuite-libre-de-droit-unsplash-1024x683.jpg', '2020-04-19 21:46:37');
 
 -- --------------------------------------------------------
 
@@ -62,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `jaime` (
   `id_article` int(11) NOT NULL,
   `nom_personne` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `jaime`
@@ -73,7 +87,13 @@ INSERT INTO `jaime` (`id`, `id_article`, `nom_personne`) VALUES
 (25, 1, ''),
 (16, 9, ''),
 (21, 14, ''),
-(28, 1, 'coby');
+(28, 1, 'coby'),
+(30, 26, 'Coby'),
+(35, 26, 'Joel'),
+(37, 26, 'Fulton'),
+(38, 29, 'Fulton'),
+(39, 27, 'Fulton'),
+(40, 30, 'Fulton');
 
 -- --------------------------------------------------------
 
@@ -89,7 +109,7 @@ CREATE TABLE IF NOT EXISTS `personne` (
   `dtN` date DEFAULT NULL,
   `mail` varchar(100) DEFAULT NULL,
   `mdp` varchar(100) DEFAULT NULL,
-  `photoProfil` varchar(100) DEFAULT NULL,
+  `photoProfil` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   UNIQUE KEY `nomUtilisateur` (`nomUtilisateur`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -98,7 +118,7 @@ CREATE TABLE IF NOT EXISTS `personne` (
 --
 
 INSERT INTO `personne` (`nomUtilisateur`, `nom`, `prenom`, `dtN`, `mail`, `mdp`, `photoProfil`) VALUES
-('Coby', 'Coby', 'Bernard', '1968-08-10', 'sollicitudin@MaurismagnaDuis.com', 'Coby', 'img/pp.png'),
+('Coby', 'Cobys', 'Bernard', '1968-08-10', 'sollicitudin@MaurismagnaDuis.com', 'Coby', 'uploads/pp/5e9caa3d03914avatar.png'),
 ('Fulton', 'Fulton', 'Zachery', '2002-12-30', 'in@rutrum.com', 'Fulton', 'img/pp.png'),
 ('Timon', 'Timon', 'Adrian', '1989-11-11', 'sollicitudin@ultrices.org', 'Timon', 'img/pp.png'),
 ('Joel', 'Joel', 'Susan', '2001-05-12', 'vel@necmalesuadaut.com', 'Joel', 'img/pp.png'),
@@ -125,28 +145,22 @@ CREATE TABLE IF NOT EXISTS `publication` (
   `photo` varchar(100) DEFAULT NULL,
   `datepubli` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `publication`
 --
 
 INSERT INTO `publication` (`id`, `nblike`, `titre`, `description`, `user`, `photo`, `datepubli`) VALUES
-(1, 0, 'test1', 'l\'ecologie c\'est cool', 'Coby', 'img/tilleul-arbre.jpg', '2020-04-18 23:07:54'),
-(6, 0, 'test3', 'test3', 'Coby', 'uploads/5e99c26c84375Forza456.jpg', '2020-04-18 23:07:54'),
-(9, 0, 'test', 'blabla', 'Coby', 'uploads/5e99c6296cb42Forza3.jpg', '2020-04-18 23:07:54'),
-(10, 0, 'dfssds', 'sdsdsd', 'Coby', 'uploads/5e99c65e15464Forza2.jpg', '2020-04-18 23:07:54'),
-(11, 0, 'test2', 'sdfds', 'Coby', 'uploads/5e99c690b3de6bmw.jpg', '2020-04-18 23:07:54'),
-(8, 0, 'bugatti', 'sd', 'Coby', 'uploads/5e99c56bdc058bugatti_chiron_super_sport_300__prototype_2019_4k_8k-HD.jpg', '2020-04-18 23:07:54'),
-(13, 0, 'test log', 'test login', 'Coby', 'uploads/5e9b6aa41b209TEEEST.jpg', '2020-04-18 23:07:54'),
-(14, 0, 'test log et date', 'sdsdsdsdsdsdsd', 'Coby', 'uploads/5e9b6c516ae8fTEEEST.jpg', '2020-04-18 23:08:33'),
-(15, 0, 'test400', 'test1', 'Coby', 'uploads/5e9b70c4d6d7cTEEEST.jpg', '2020-04-18 23:27:32'),
-(16, 0, 'test', 'sd', 'Coby', 'uploads/5e9b713a985f4TEEEST.jpg', '2020-04-18 23:29:30'),
-(17, 0, 'test', 'sd', 'Coby', 'uploads/5e9b715d39296TEEEST.jpg', '2020-04-18 23:30:05'),
-(18, 0, 'test', 'sd', 'Coby', 'uploads/5e9b71776491bTEEEST.jpg', '2020-04-18 23:30:31'),
-(19, 0, 'test', 'sd', 'Coby', 'uploads/5e9b71787f8f9TEEEST.jpg', '2020-04-18 23:30:32'),
-(20, 0, 'test', 'sd', 'Coby', 'uploads/5e9b7184c5fb2TEEEST.jpg', '2020-04-18 23:30:44'),
-(21, 0, 'test', 'sd', 'Coby', 'uploads/5e9b71b7a85c9TEEEST.jpg', '2020-04-18 23:31:35');
+(30, 0, 'test1', 'test3', 'Fulton', 'uploads/5e9c33e42a1209-4.jpg', '2020-04-19 13:20:04'),
+(32, 0, 'test', 'sd', 'Cobys', 'uploads/5e9caabfd1bfcphoto-gratuite-libre-de-droit-unsplash-1024x683.jpg', '2020-04-19 21:47:11'),
+(29, 0, 'test2', 'test3', 'Fulton', 'uploads/5e9c33da3fed8images.jpg', '2020-04-19 13:19:54'),
+(28, 0, 'test1', 'sd', 'Joel', 'uploads/5e9c3393cd229téléchargement.jpg', '2020-04-19 13:18:43'),
+(27, 0, 'bugatti', 'sd', 'Joel', 'uploads/5e9c32900de3cimages (2).jpg', '2020-04-19 13:14:24'),
+(26, 0, 'hip', 'hop', 'Joel', 'uploads/5e9c303b0e72bimages-3.jpg', '2020-04-19 13:04:27'),
+(25, 0, 'wouah', 'ouf', 'Joel', 'uploads/5e9c3029f3c02images (1).jpg', '2020-04-19 13:04:10'),
+(24, 0, 'test', 'blabla', 'Cobys', 'uploads/5e9c2fffa5e60photo-gratuite-libre-de-droit-unsplash-1024x683.jpg', '2020-04-19 13:03:27'),
+(31, 0, 'dssdsd', 'sdsddd', 'Fulton', 'uploads/5e9c3898b89e4banque-d-images-gratuites-et-libres-de-droits18-1560x1037.jpg', '2020-04-19 13:40:08');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
