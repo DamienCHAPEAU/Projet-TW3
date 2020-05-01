cda6c08bfef0f915fd83624e12d582dc6b2ed5bd
ClementATTIE <55447843+ClementATTIE@users.noreply.github.com>
2020-05-02 01:23:33 +0200

    localisation maj

--- Gretagram/carte.php
+++ Gretagram/carte.php
@@ -30,7 +30,6 @@ if (isset($_SESSION['login']) && isset($_SESSION['mdp'])) {
 <head>
     <meta charset="utf-8">
     <title>Gretagram</title>
-
     <!--Font-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <!--FontAwesome-->
@@ -44,8 +43,6 @@ if (isset($_SESSION['login']) && isset($_SESSION['mdp'])) {
     <!--JS-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
-
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
 
@@ -107,9 +104,6 @@ if (isset($_SESSION['login']) && isset($_SESSION['mdp'])) {
     <br>
     
     </div>
-
-
-
     <div id="mapid" style="width: 100%; height: 75%; z-index: 10;"></div>
 
     <script src="script/L.Control.Locate.js" ></script>
@@ -210,6 +204,7 @@ if (isset($_SESSION['login']) && isset($_SESSION['mdp'])) {
     ?>
 
     <script>
+/*
         L.circle([51.508, -0.11], 500, {
             color: 'red',
             fillColor: '#f03',
@@ -227,6 +222,7 @@ if (isset($_SESSION['login']) && isset($_SESSION['mdp'])) {
         });
 
         var popup = L.popup();
+*/
     </script>
 
 
